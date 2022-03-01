897efa6d674215c8270d51d7f3235f829190a359
shhmabbey <abhernndz@gmail.com>
2022-02-28 16:10:10 -0800

    Switch main program to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
   entry: [
-    path.resolve(__dirname, 'src', 'v2.js'),
+    path.resolve(__dirname, 'src', 'index.js'),
     path.resolve(__dirname, 'src', 'index.scss'),
   ],
   output: {
